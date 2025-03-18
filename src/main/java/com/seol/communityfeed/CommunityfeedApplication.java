2f1630a209c5e59faa2a5cb8d6448d27a94e4ebf
윤설이 <olerlmin1234@gmail.com>
2025-03-18 15:53:18 +0900

    유저 생성

---
+++ src/main/java/com/seol/communityfeed/CommunityfeedApplication.java
@@ -0,0 +1,15 @@
+package com.seol.communityfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityfeedApplication {
+
+    public static void main(String[] args) {
+
+        //SpringApplication.run(CommunityfeedApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
